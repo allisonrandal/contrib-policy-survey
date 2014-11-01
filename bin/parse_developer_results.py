44791de81f06474baa85d41ee5db0b3a47170c0e
Allison Randal <allison.randal@hp.com>
2014-11-01 13:49:38 -0700

    Refine calculations for contribution history to include more types of contribution.

--- bin/parse_developer_results.py
+++ bin/parse_developer_results.py
@@ -3,7 +3,7 @@
 import csv
 
 stats = {}
-stats['signed'] = {'license': 0, 'assign': 0, 'dco': 0, 'other': 0, 'ltotal': 0, 'atotal': 0, 'dtotal': 0, 'ototal': 0}
+stats['signed'] = {}
 stats['future'] = {
     'license': {
         'total': 0,
@@ -25,37 +25,30 @@ stats['future'] = {
     },
 }
 
+past_fields = {
+    'assignmentsigned': 'CAA',
+    'licensesigned': 'CLA',
+    'dcosigned': 'DCO',
+    'othersigned': 'Other',
+    'othersame': 'in=out',
+    'othernopolicy': 'No policy'
+}
+
 # Calculate stats from survey results
 with open('data/developer_survey_results.csv', 'rb') as csvfile:
     results = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     for row in results:
-        if row['licensesigned'] == 'Yes':
-            stats['signed']['license'] += 1
-            stats['signed']['ltotal'] += 1
-
-        if row['licensesigned'] == 'No':
-            stats['signed']['ltotal'] += 1
-
-        if row['assignmentsigned'] == 'Yes':
-            stats['signed']['assign'] += 1
-            stats['signed']['atotal'] += 1
-
-        if row['assignmentsigned'] == 'No':
-            stats['signed']['atotal'] += 1
-
-        if row['dcosigned'] == 'Yes':
-            stats['signed']['dco'] += 1
-            stats['signed']['dtotal'] += 1
-
-        if row['dcosigned'] == 'No':
-            stats['signed']['dtotal'] += 1
-
-        if row['othersigned'] == 'Yes':
-            stats['signed']['other'] += 1
-            stats['signed']['ototal'] += 1
-
-        if row['othersigned'] == 'No':
-            stats['signed']['ototal'] += 1
+        for keyname in past_fields:
+            resultkey = past_fields[keyname]
+            if not resultkey in stats['signed']:
+                stats['signed'][resultkey] = {'yes': 0, 'total': 0}
+            if row[keyname] == 'Yes':
+                stats['signed'][resultkey]['yes'] += 1
+                stats['signed'][resultkey]['total'] += 1
+            if row[keyname] == 'No':
+                stats['signed'][resultkey]['total'] += 1
+            if row[keyname] == "I don't know":
+                stats['signed'][resultkey]['total'] += 1
 
         for keyname in stats['future']:
             cellvalue = row[keyname+'willing']
@@ -75,21 +68,13 @@ with open('data/developer_survey_results.csv', 'rb') as csvfile:
 dat_file = open('graphs/signed.dat', 'w')
 dat_file.write("# type,signed,total,percentage\n")
 
-license_percentage = float(stats['signed']['license']) / float(stats['signed']['ltotal']) * 100
-line = "CLA,%d,%d,%.2f\n" % (stats['signed']['license'], stats['signed']['ltotal'], license_percentage)
-dat_file.write(line)
-
-assign_percentage = float(stats['signed']['assign']) / float(stats['signed']['atotal']) * 100
-line = "CAA,%d,%d,%.2f\n" % (stats['signed']['assign'], stats['signed']['atotal'], assign_percentage)
-dat_file.write(line)
-
-dco_percentage = float(stats['signed']['dco']) / float(stats['signed']['dtotal']) * 100
-line = "DCO,%d,%d,%.2f\n" % (stats['signed']['dco'], stats['signed']['dtotal'], dco_percentage) 
-dat_file.write(line)
-
-other_percentage = float(stats['signed']['other']) / float(stats['signed']['ototal']) * 100
-line = "Other,%d,%d,%.2f\n" % (stats['signed']['other'], stats['signed']['ototal'], other_percentage) 
-dat_file.write(line)
+past_columns = ['CAA', 'CLA', 'DCO', 'in=out', 'No policy', 'Other' ]
+for keyname in past_columns:
+    print 'processing ' + keyname
+    print 'total is %d ' % stats['signed'][keyname]['total']
+    percentage = float(stats['signed'][keyname]['yes']) / float(stats['signed'][keyname]['total']) * 100
+    line = "%s,%d,%d,%.2f\n" % (keyname,stats['signed'][keyname]['yes'], stats['signed'][keyname]['total'], percentage)
+    dat_file.write(line)
 
 dat_file.close()
 
--- graphs/graph_signed.gp
+++ graphs/graph_signed.gp
@@ -2,11 +2,11 @@ set terminal pngcairo enhanced font "DroidSans,10" size 640, 200
 set output "signed.png"
 set datafile separator ","
 
-set title "Percentage of developers who have contributed under an agreement"
+set title "Percentage of developers who have contributed in the past"
 set size ratio 0.25
 set offsets 0.1, 0.1, 1, 1
 
-set style line 1 lc rgb "#2b83ba" lt 1 linewidth 1.3
+set style line 1 lc rgb "#2166ac" lt 1 linewidth 1.3
 set style line 11 lc rgb "#000000" lt 1
 set border 3 linestyle 11
 set tics nomirror
